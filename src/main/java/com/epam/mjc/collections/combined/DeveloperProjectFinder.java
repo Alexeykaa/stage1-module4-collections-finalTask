c4d81c679a02780f126d833a034c4ae408dbccfb
Alexeykaa <ka.alexey@gmail.com>
2023-10-22 23:02:26 +0300

    init implementation

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,26 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        LinkedList<String> result = new LinkedList<>();
+        for (Map.Entry<String, Set<String>> e : projects.entrySet()) {
+            Set<String> developers = e.getValue();
+            if (developers.contains(developer)) {
+                result.add(e.getKey());
+            }
+        }
+        result.sort(new Comparator<>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int cmp = o2.length() - o1.length();
+                if (cmp == 0) {
+                    cmp = o2.compareTo(o1);
+                }
+                return cmp;
+            }
+        });
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,20 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        HashMap<Integer, Set<String>> result = new HashMap<>();
+        for(Map.Entry<String, Integer> e : sourceMap.entrySet()) {
+            String key = e.getKey();
+            int length = key.length();
+            Set<String> keys = result.get(length);
+            if (keys == null) {
+                keys = new HashSet<>();
+            }
+            keys.add(key);
+            result.put(length, keys);
+        }
+        return result;
     }
 }
